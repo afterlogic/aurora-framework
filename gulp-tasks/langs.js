800032dde443a37e445c36b6e3894057f1cddb9a
Nadine <nadine@afterlogic.com>
2016-02-19 12:12:43 +0300

    JS - Core - bundle ini language files from all modules to 1 json language file

---
+++ gulp-tasks/langs.js
@@ -0,0 +1,114 @@
+var
+	_ = require('underscore'),
+	buffer = require('vinyl-buffer'),
+	fileExists = require('file-exists'),
+	gulp = require('gulp'),
+	ini = require('cascade-ini'),
+	source = require('vinyl-source-stream')
+;
+
+/**
+ * 
+ * @param {string} sParamName
+ * @param {string} sDefault
+ * @returns {string}
+ */
+function GetParameterFromArgv(sParamName, sDefault)
+{
+	var sParamValue = sDefault || '';
+	
+	_.each(process.argv, function (sArg, iIndex) {
+		if (sArg === sParamName && process.argv[iIndex + 1])
+		{
+			sParamValue = process.argv[iIndex + 1];
+		}
+	});
+	
+	return sParamValue;
+}
+
+function GetModulesFromArgv()
+{
+	var sModules = GetParameterFromArgv('--modules');
+	
+	return _.union(['Core'], sModules.split(','));
+}
+
+gulp.task('langs', function() {
+	var
+		sLanguage = GetParameterFromArgv('--lang', 'English'),
+		aModulesNames = GetModulesFromArgv(),
+		aModules = _.map(aModulesNames, function (sModuleName) {
+			var sFilePath = './modules/' + sModuleName + '/i18n/' + sLanguage + '.ini';
+			return fileExists(sFilePath) ? sFilePath : './modules/' + sModuleName + '/i18n/English.ini';
+		}),
+		stream = source(sLanguage + '.json'),
+		iCount = 0,
+		iTotal = aModules.length,
+		fEndStream = function () {
+			if (iCount === iTotal)
+			{
+				stream.write('\r\n}');
+				process.nextTick(function() {
+					stream.end();
+				});
+				stream
+					.pipe(buffer())
+					.pipe(gulp.dest('./static/i18n/'));
+			}
+		}
+	;
+	
+	stream.write('{\r\n');
+	
+	_.each(aModules, function (sModulePath, iModuleIndex) {
+		ini.parseFile(sModulePath, function(oError, oData) {
+			if (oError)
+			{
+				console.log('Error', oError);
+			}
+			else
+			{
+				var
+					sPrefix = aModulesNames[iModuleIndex].toUpperCase(),
+					sConstants = ''
+				;
+
+				_.each(oData, function (sItem, sKey) {
+					var bInQuotes = sItem.substr(0, 1) === '"';
+					while(sItem.substr(0, 1) === '"' || sItem.substr(0, 1) === ' ')
+					{
+						sItem = sItem.substr(1);
+					}
+					while (sItem.substr(-1, 1) === '"' || sItem.substr(-1, 1) === ' ')
+					{
+						sItem = sItem.substr(0, sItem.length - 1);
+					}
+					if (!bInQuotes)
+					{
+						sItem = sItem.replace(/\"/g, '\\"').replace(/\r\n/g, '\\r\\n');
+					}
+					sConstants += '"' + sPrefix + '/' + sKey + '": "' + sItem + '",\r\n';
+				});
+
+				if (iCount < iTotal - 1)
+				{
+					if (sConstants !== '')
+					{
+						stream.write(sConstants + '\r\n');
+					}
+				}
+				else
+				{
+					stream.write(sConstants.slice(0, -3));
+				}
+			}
+
+			iCount++;
+
+			fEndStream();
+		});
+	});
+});
+
+module.exports = {};
