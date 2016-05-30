38e1a49c2e2b38466c9ebe04a95e8f4937a4ecc3
Nadine <nadine@afterlogic.com>
2016-05-30 12:38:27 +0300

    SimpleChatLogger Module

--- modules/SimpleChatLogger/index.php
+++ modules/SimpleChatLogger/index.php
@@ -2,11 +2,21 @@
 
 class SimpleChatLoggerModule extends AApiModule
 {
+	/**
+	 * Subscribes on the event that is broadcasted after executing of the CreatePost method in the SimpleChat module.
+	 */
 	public function init()
 	{
-		$this->subscribeEvent('CreatePost::after', array($this, 'afterCreatePost'));
+		$this->subscribeEvent('SimpleChat::CreatePost::after', array($this, 'afterCreatePost'));
 	}
 	
+	/**
+	 * Logs data of the user post in the simple chat.
+	 * 
+	 * @param array $aArgs Array with data of the user post in the simple chat.
+	 *  ['Date'] - string Date and time when user put down the post in UTC.
+	 *  ['Text'] - string Text of the user post.
+	 */
 	public function afterCreatePost($aArgs)
 	{
 		$iUserId = \CApi::getLogginedUserId();
