31aa58921a2fbeaebb509b4ee80f33b77512c4e1
sash04ek <a.ovcharov@gmail.com>
2016-06-17 12:36:08 +0300

    update EAV Manager

--- migration/update.sql
+++ migration/update.sql
@@ -1,24 +1,82 @@
-CREATE TABLE IF NOT EXISTS `eav_objects` (
-  `id` int(11) NOT NULL auto_increment,
-  `object_type` varchar(255) default NULL,
-  `module_name` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `eav_objects`;
+DROP TABLE IF EXISTS `eav_properties`;
 
-CREATE TABLE IF NOT EXISTS `eav_properties` (
-  `id` int(11) NOT NULL auto_increment,
-  `id_object` int(11) default NULL,
-  `key` varchar(255) default NULL,
-  `type` varchar(255) default NULL,
-  `value_text` text,
-  `value_string` varchar(255) default NULL,
-  `value_int` int(11) default NULL,
-  `value_bool` tinyint(1) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- ----------------------------------------------------------------------------
+-- Table eav_attributes_bool
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_attributes_bool` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_entity` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `value` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idx_unique` (`id_entity` ASC, `name` ASC),
+  INDEX `idx_value` (`value` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `eav_properties` 
-ADD UNIQUE INDEX `id_object_key` (`id_object` ASC, `key` ASC);
+-- ----------------------------------------------------------------------------
+-- Table eav_attributes_datetime
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_attributes_datetime` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_entity` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `value` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idx_unique` (`id_entity` ASC, `name` ASC),
+  INDEX `idx_value` (`value` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `eav_properties`
-ADD COLUMN `value_datetime` DATETIME NULL AFTER `value_bool`;
+-- ----------------------------------------------------------------------------
+-- Table eav_attributes_int
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_attributes_int` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_entity` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `value` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idx_unique` (`id_entity` ASC, `name` ASC),
+  INDEX `idx_value` (`value` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- ----------------------------------------------------------------------------
+-- Table eav_attributes_string
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_attributes_string` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_entity` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `value` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idx_unique` (`id_entity` ASC, `name` ASC),
+  INDEX `idx_value` (`value` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- ----------------------------------------------------------------------------
+-- Table eav_attributes_text
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_attributes_text` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_entity` INT(11) NULL DEFAULT NULL,
+  `name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `value` TEXT CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idx_unique` (`id_entity` ASC, `name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- ----------------------------------------------------------------------------
+-- Table eav_entities
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eav_entities` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `entity_type` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `module_name` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
