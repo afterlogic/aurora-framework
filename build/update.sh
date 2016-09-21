0c3642108c8ae3fa3b21bcf7a3013da9b7756595
vasil <vasil.sokolov@gmail.com>
2016-09-21 17:56:37 +0300

    added static builder calls to update script

--- build/update.sh
+++ build/update.sh
@@ -13,7 +13,24 @@ do
 done
 
 cd .. > /dev/null;
-gulp all;
-./_build-styles.bat;
-./_build-langs.bat;
+
+
+#Build static data
+
+Modules=();
+for dir in *;
+do
+	if [ -d "$dir" ];then
+		Modules=("${Modules[@]}" "$dir");
+	fi
+done
+
+cd .. > /dev/null;
+# gulp all;
+# ./_build-styles.bat;
+node ./node_modules/gulp/bin/gulp.js styles --modules $(IFS=, ; echo "${Modules[*]}") --themes Default,Funny
+# ./_build-langs.bat;
+node ./node_modules/gulp/bin/gulp.js langs --modules $(IFS=, ; echo "${Modules[*]}") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
+# ./_build-js.bat;
+node ./node_modules/gulp/bin/gulp.js js1:build --output app --modules $(IFS=, ; echo "${Modules[*]}")
 
