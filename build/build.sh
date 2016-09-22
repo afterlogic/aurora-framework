5a2c0afbf86bb6e0cb3ef3ebcd401cbafad9551b
sash04ek <a.ovcharov@gmail.com>
2016-09-22 12:15:31 +0300

    update buil script

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+read -p "Tenant name: " response
+
+tenant=""
+if [ "$response" != "" ]; 
+then
+	tenant=" --tenant $response";
+fi
+
 #Build static data
 cd ../modules
 
@@ -11,6 +19,6 @@ do
 	fi
 done
 
-gulp styles --modules $(IFS=, ; echo "${Modules[*]}") --themes Default,Funny
-gulp langs --modules $(IFS=, ; echo "${Modules[*]}") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
-gulp js1:build --output app --modules $(IFS=, ; echo "${Modules[*]}")
+gulp styles --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --themes Default,Funny --tenant $response
+gulp langs --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
+gulp js1:build --output app $(echo "$tenant") --modules $(IFS=, ; echo "${Modules[*]}")
