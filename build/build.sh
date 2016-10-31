20b6a28e54bcee2e0f555515e234638a4a3463de
vasil <vasil.sokolov@gmail.com>
2016-10-31 14:51:57 +0300

    updated build script

--- build/build.sh
+++ build/build.sh
@@ -28,8 +28,10 @@ done
 gulp styles --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --themes Default,Funny
 gulp langs --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
 gulp js:build --output app $(echo "$tenant") --modules $(IFS=, ; echo "${Modules[*]}")
+gulp js:min --output app $(echo "$tenant") --modules $(IFS=, ; echo "${Modules[*]}")
 
 if [ "$pubFile" != "" ]; 
 then
 	gulp js:build --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
+	gulp js:min --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
 fi
