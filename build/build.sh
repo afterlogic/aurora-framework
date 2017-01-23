7d5afabae42d9c8723255e2e82e22c6263a70b4c
vasil <vasil.sokolov@gmail.com>
2017-01-23 12:29:29 +0300

    removed building js public files from build.sh

--- build/build.sh
+++ build/build.sh
@@ -8,11 +8,11 @@ then
 	tenant=" --tenant $response";
 fi
 
-read -p "Pub file name: " pubFile
-if [ "$pubFile" != "" ]; 
-then
-	read -p "Modules: " pubModules
-fi
+# read -p "Pub file name: " pubFile
+# if [ "$pubFile" != "" ]; 
+# then
+	# read -p "Modules: " pubModules
+# fi
 
 #Build static data
 cd ../modules
@@ -26,12 +26,12 @@ do
 done
 
 gulp styles --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --themes Default,Funny
-gulp langs --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
+# gulp langs --modules $(IFS=, ; echo "${Modules[*]}") $(echo "$tenant") --langs Arabic,Bulgarian,Chinese-Simplified,Chinese-Traditional,Czech,Danish,Dutch,English,Estonian,Finnish,French,German,Greek,Hebrew,Hungarian,Italian,Japanese,Korean,Latvian,Lithuanian,Norwegian,Persian,Polish,Portuguese-Brazil,Portuguese-Portuguese,Romanian,Russian,Serbian,Slovenian,Spanish,Swedish,Thai,Turkish,Ukrainian,Vietnamese
 gulp js:build --output app $(echo "$tenant") --modules $(IFS=, ; echo "${Modules[*]}")
 gulp js:min --output app $(echo "$tenant") --modules $(IFS=, ; echo "${Modules[*]}")
 
-if [ "$pubFile" != "" ]; 
-then
-	gulp js:build --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
-	gulp js:min --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
-fi
+# if [ "$pubFile" != "" ]; 
+# then
+	# gulp js:build --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
+	# gulp js:min --output $(echo "$pubFile") $(echo "$tenant") --modules $(echo "$pubModules")
+# fi
