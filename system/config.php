9eff6f8c7392bedd6cadd4cc282f2de6b7ce99b9
vasil <vasil.sokolov@gmail.com>
2017-01-30 17:09:45 +0300

    mark unused config params

--- system/config.php
+++ system/config.php
@@ -39,7 +39,7 @@ return array(
 	/**
 	 * The setting defines size of the log file extract available through adminpanel (in Kbytes)
 	 */
-	'log.max-view-size' => 1000,
+	//unused 'log.max-view-size' => 1000,
 
 	/**
 	 * Socket connection timeout limit (in seconds)
@@ -56,18 +56,18 @@ return array(
 	/**
 	 * Encoding used for composing mails
 	 */
-	'webmail.default-out-charset' => 'utf-8',
+	//unused 'webmail.default-out-charset' => 'utf-8',
 
 	/**
 	 * Defines whether messages are prefetched to minimize response time
 	 * when selecting a message, WebMail Pro fetches messages from server in background.
 	 */
-	'webmail.use-prefetch' => true,
+	//unused 'webmail.use-prefetch' => true,
 
 	/**
 	 * Languages considered to be RTL ones by WebMail
 	 */
-	'webmail.rtl-langs' => array('Hebrew', 'Arabic', 'Persian'),
+	//unused 'webmail.rtl-langs' => array('Hebrew', 'Arabic', 'Persian'),
 
 	/**
 	 * X-Mailer value used in outgoing mails
@@ -80,12 +80,12 @@ return array(
 	 * WebMail attempts to locate special (system) folders like Trash, Drafts, Sent Items.
 	 * If particular folder is not found, WebMail can create it, and you can disable this of course.
 	 */
-	'webmail.create-imap-system-folders' => true,
+	//unused 'webmail.create-imap-system-folders' => true,
 
 	/**
 	 * Configuration option for creating required folders on each login.
 	 */
-	'webmail.system-folders-sync-on-each-login' => false,
+	//unused 'webmail.system-folders-sync-on-each-login' => false,
 
 	/**
 	 * IMAP4 only
@@ -97,35 +97,35 @@ return array(
 	/**
 	 * Memory limit set by WebMail for resource-consuming operations (in Mbytes)
 	 */
-	'webmail.memory-limit' => 200,
+	//unused 'webmail.memory-limit' => 200,
 
 	/**
 	 * Time limit set by WebMail for resource-consuming operations (in seconds)
 	 */
-	'webmail.time-limit' => 3000,
+	//unused 'webmail.time-limit' => 3000,
 
 	/**
 	 * Enable saving drafts automatically. Saving is performed once a minute,
 	 * assuming it is supported by particular IMAP server.
 	 * Default value: true
 	 */
-	'webmail.autosave' => true,
+	//unused 'webmail.autosave' => true,
 
 	/**
 	 * Enable joining reply prefixes when subject of the answer is formed.
 	 * Default value: true
 	 */
-	'webmail.join-reply-prefixes' => true,
+	//unused 'webmail.join-reply-prefixes' => true,
 
 	/**
 	 * Enable browsers to add WebMail as an application for mailto links.
 	 * Default value: true
 	 */
-	'webmail.allow-app-register-mailto' => true,
+	//unused 'webmail.allow-app-register-mailto' => true,
 	
 	'mailsuite' => false,
 
-	'files' => true,
+	//unused 'files' => true,
 
 	'tenant' => false,
 
@@ -133,11 +133,11 @@ return array(
 
 	'capa' => false,
 
-	'themes' => array('Default', 'DeepForest', 'OpenWater', 'Funny', 'BlueJeans', 'White'),
+	//unused 'themes' => array('Default', 'DeepForest', 'OpenWater', 'Funny', 'BlueJeans', 'White'),
 
-	'links.outlook-sync-plugin-32' => 'http://www.afterlogic.com/download/OutlookSyncAddIn.msi',
-	'links.outlook-sync-plugin-64' => 'http://www.afterlogic.com/download/OutlookSyncAddIn64.msi',
-	'links.outlook-sync-read-more' => 'http://www.afterlogic.com/wiki/Outlook_sync_(Aurora)',
+	//unused 'links.outlook-sync-plugin-32' => 'http://www.afterlogic.com/download/OutlookSyncAddIn.msi',
+	//unused 'links.outlook-sync-plugin-64' => 'http://www.afterlogic.com/download/OutlookSyncAddIn64.msi',
+	//unused 'links.outlook-sync-read-more' => 'http://www.afterlogic.com/wiki/Outlook_sync_(Aurora)',
 
 	/*
 	 * temp.cron-time-*
@@ -160,105 +160,105 @@ return array(
 	 */
 	'temp.cron-time-file' => '.clear.dat',
 
-	'langs.names' => array(
-		'Arabic' => 'العربية',
-		'Bulgarian' => 'Български',
-		'Chinese-Simplified' => '中文(简体)',
-		'Chinese-Traditional' => '中文(香港)',
-		'Czech' => 'Čeština',
-		'Danish' => 'Dansk',
-		'Dutch' => 'Nederlands',
-		'English' => 'English',
-		'Estonian' => 'eesti',
-		'Finnish' => 'Suomi',
-		'French' => 'Français',
-		'German' => 'Deutsch',
-		'Greek' => 'Ελληνικά',
-		'Hebrew' => 'עברית',
-		'Hungarian' => 'Magyar',
-		'Italian' => 'Italiano',
-		'Japanese' => '日本語',
-		'Korean' => '한국어',
-		'Latvian' => 'Latviešu',
-		'Lithuanian' => 'Lietuvių',
-		'Norwegian' => 'Norsk',
-		'Persian' => 'فارسی',
-		'Polish' => 'Polski',
-		'Portuguese-Portuguese' => 'Português',
-		'Portuguese-Brazil' => 'Português Brasileiro',
-		'Romanian' => 'Română',
-		'Russian' => 'Русский',
-		'Serbian' => 'Srpski',
-		'Slovenian' => 'Slovenščina',
-		'Spanish' => 'Español',
-		'Swedish' => 'Svenska',
-		'Thai' => 'ภาษาไทย',
-		'Turkish' => 'Türkçe',
-		'Ukrainian' => 'Українська',
-		'Vietnamese' => 'tiếng Việt'
-	),
+	//unused 'langs.names' => array(
+//		'Arabic' => 'العربية',
+//		'Bulgarian' => 'Български',
+//		'Chinese-Simplified' => '中文(简体)',
+//		'Chinese-Traditional' => '中文(香港)',
+//		'Czech' => 'Čeština',
+//		'Danish' => 'Dansk',
+//		'Dutch' => 'Nederlands',
+//		'English' => 'English',
+//		'Estonian' => 'eesti',
+//		'Finnish' => 'Suomi',
+//		'French' => 'Français',
+//		'German' => 'Deutsch',
+//		'Greek' => 'Ελληνικά',
+//		'Hebrew' => 'עברית',
+//		'Hungarian' => 'Magyar',
+//		'Italian' => 'Italiano',
+//		'Japanese' => '日本語',
+//		'Korean' => '한국어',
+//		'Latvian' => 'Latviešu',
+//		'Lithuanian' => 'Lietuvių',
+//		'Norwegian' => 'Norsk',
+//		'Persian' => 'فارسی',
+//		'Polish' => 'Polski',
+//		'Portuguese-Portuguese' => 'Português',
+//		'Portuguese-Brazil' => 'Português Brasileiro',
+//		'Romanian' => 'Română',
+//		'Russian' => 'Русский',
+//		'Serbian' => 'Srpski',
+//		'Slovenian' => 'Slovenščina',
+//		'Spanish' => 'Español',
+//		'Swedish' => 'Svenska',
+//		'Thai' => 'ภาษาไทย',
+//		'Turkish' => 'Türkçe',
+//		'Ukrainian' => 'Українська',
+//		'Vietnamese' => 'tiếng Việt'
+//	),
 
 	/**
 	 * Enable plugins in WebMail
 	 */
-	'plugins' => true,
+	//unused 'plugins' => true,
 
 	/**
 	 * Force enabling all the plugins.
 	 */
-	'plugins.config.include-all' => false,
+	//unused 'plugins.config.include-all' => false,
 
 	// labs.*
 	// Experimental settings
 	'labs.db.use-explain' => false,
 	'labs.db.use-explain-extended' => false,
 	'labs.db.log-query-params' => false,
-	'labs.htmleditor-default-font-name' => '',
-	'labs.htmleditor-default-font-size' => '',
+	//unused 'labs.htmleditor-default-font-name' => '',
+	//unused 'labs.htmleditor-default-font-size' => '',
 	'labs.log.post-view' => true,
-	'labs.allow-social-integration' => true,
+	//unused 'labs.allow-social-integration' => true,
 	'labs.use-app-min-js' => true,
-	'labs.webmail.gmail-fix-folders' => true,
-	'labs.webmail.custom-login-url' => '',
-	'labs.webmail.custom-logout-url' => '',
-	'labs.webmail.disable-folders-manual-sort' => false,
-	'labs.webmail.ios-detect-on-login' => true,
+	//unused 'labs.webmail.gmail-fix-folders' => true,
+	//unused 'labs.webmail.custom-login-url' => '',
+	//unused 'labs.webmail.custom-logout-url' => '',
+	//unused 'labs.webmail.disable-folders-manual-sort' => false,
+	//unused 'labs.webmail.ios-detect-on-login' => true,
 	'labs.webmail.display-server-error-information' => false,
 	'labs.webmail.display-inline-css' => false,
 	'labs.allow-thumbnail' => true,
 	'labs.allow-post-login' => false,
-	'labs.allow-save-as-pdf' => false,
+	//unused 'labs.allow-save-as-pdf' => false,
 	'labs.dav.use-browser-plugin' => false,
-	'labs.dav.use-export-plugin' => true,
-	'labs.dav.use-files' => false,
+	//unused 'labs.dav.use-export-plugin' => true,
+	//unused 'labs.dav.use-files' => false,
 	'labs.dav.admin-principal' => 'principals/admin',
-	'labs.cache.i18n' => true,
+	//unused 'labs.cache.i18n' => true,
 	'labs.cache.templates' => true,
 	'labs.cache.static' => true,
 	'labs.twilio' => false,
 	'labs.voice' => false,
-	'labs.open-pgp' => true,
+	//unused 'labs.open-pgp' => true,
 	'labs.webmail.csrftoken-protection' => true,
-	'labs.webmail-client-debug' => false,
+	//unused 'labs.webmail-client-debug' => false,
 	'labs.x-frame-options' => '',
-	'labs.fetchers' => true,
-	'labs.simple-saas-api-key' => '',
+	//unused 'labs.fetchers' => true,
+	//unused 'labs.simple-saas-api-key' => '',
 	'labs.message-body-size-limit' => 25000,
-	'labs.unlim-quota-limit-size-in-kb' => 104857600,
+	//unused 'labs.unlim-quota-limit-size-in-kb' => 104857600,
 	'labs.use-date-from-headers' => false,
 	'labs.use-body-structures-for-has-attachments-search' => false,
-	'labs.google-analytic.account' => '',
+	//unused 'labs.google-analytic.account' => '',
 	'labs.app-cookie-path' => '/',
 	'labs.prefer-starttls' => true,
 	'labs.server-use-url-rewrite' => false,
 	'labs.server-url-rewrite-base' => '',
 	'labs.db-debug-backtrace-limit' => 0,
 	'labs.allow-officeapps-viewer' => true,
-	'labs.mail-expand-folders' => false,
+	//unused 'labs.mail-expand-folders' => false,
 	'labs.i18n' => 'en',
 	
 	/* Enable Social Auth plugin */
-	'plugins.external-services' => true,
+	//unused'plugins.external-services' => true,
 /*	'plugins.external-services.connectors' => array(
 		'google',
 		'dropbox',
