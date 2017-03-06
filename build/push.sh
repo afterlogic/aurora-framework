93a2f9daee54067214201455574cdc7ba187337f
sash04ek <a.ovcharov@gmail.com>
2017-03-06 10:36:29 +0300

    sdfgsdfgsdfg

--- build/push.sh
+++ build/push.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-read -p "Commit message: " response
+read -p "Commit message: " commit
+read -p "GitHub Login: " login
+read -p "GitHub Password: " password
 
 echo "Aurora Core";
-git add -A;
-git commit -m $response;
-git push origin/master;
-echo "";
+git add -A
+git commit -m $commit;
 
-cd ../modules
+url = "${git config --get remote.origin.url}"
 
-for dir in $(find . -name ".git");
-do
-    cd ${dir%/*} > /dev/null;
-    echo ${dir%/*};
-    git add -A;
-	git commit -m $response;
-	git push origin/master;
-    cd -  > /dev/null;
-done
+echo $url
+
+#git push origin/master
+#echo "";
+
+#cd ../modules
+
+#for dir in $(find . -name ".git")
+#do
+#    cd ${dir%/*} > /dev/null
+#    echo ${dir%/*}
+#    git add -A
+#	git commit -m $response
+#	git push origin/master
+#    cd -  > /dev/null
+#done
