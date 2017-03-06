01a810b1ebd1bc0f1016c3aa853fbd36f3bf1ff2
sash04ek <a.ovcharov@gmail.com>
2017-03-06 12:00:44 +0300

    test

--- build/push.sh
+++ build/push.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
-read -p "Commit message: " commit
+update_push () 
+{
+	git add  -A
+	git commit -m $3;
+	git pull
+
+	loginWithPassword=$1":"$2"@github.com"
+	loginWithAt=$1"@"
+	emptyString=""
+	guthubString="github.com"
+
+	url="$(git config --get remote.origin.url)"
+	url="${url/$loginWithAt/$emptyString}"
+	resultUrl="${url/$guthubString/$loginWithPassword}"
+
+	git push --repo $resultUrl
+} 
+
+
 read -p "GitHub Login: " login
 read -p "GitHub Password: " password
+read -p "Commit message: " commit
+
+
 
 echo "Aurora Core";
-git add -A
-git commit -m $commit;
 
-url = "${git config --get remote.origin.url}"
+update_push $login $password $commit
 
-echo $url
+echo "";
 
-#git push origin/master
-#echo "";
+cd ../modules
 
-#cd ../modules
+for dir in $(find . -name ".git")
+do
+    cd ${dir%/*} > /dev/null
+    echo ${dir%/*}
+	
+	update_push $login $password $commit
 
-#for dir in $(find . -name ".git")
-#do
-#    cd ${dir%/*} > /dev/null
-#    echo ${dir%/*}
-#    git add -A
-#	git commit -m $response
-#	git push origin/master
-#    cd -  > /dev/null
-#done
+    cd -  > /dev/null
+done
