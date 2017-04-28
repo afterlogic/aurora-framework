7f448a7e90813191ff81a7218bfe7972a9743061
vasil <vasil.sokolov@gmail.com>
2017-04-28 14:35:10 +0300

    improved pack.sh

--- build/pack.sh
+++ build/pack.sh
@@ -2,10 +2,16 @@
 
 cd ../
 
+now=$(date +"%d-%m-%Y")
+
+echo $now > VERSION
+
 #empty data folder
 #zip -r aurora-cloud.zip data modules static system vendor ".htaccess" dav.php common.php index.php LICENSE favicon.ico robots.txt -x ./data/**\*
 
+
 #data/settings only
-zip -r aurora-cloud.zip data/settings/modules modules static system vendor ".htaccess" dav.php common.php index.php LICENSE favicon.ico robots.txt
+zip -r aurora-cloud.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION favicon.ico robots.txt -x **/*.bak
+
 
 cd ./build
