a86b0fd4262cc0e7001a150d826130bc9eb386df
vasil <vasil.sokolov@gmail.com>
2017-05-05 15:17:53 +0300

    updated package building script

--- build/pack.sh
+++ build/pack.sh
@@ -11,7 +11,7 @@ echo $now > VERSION
 
 
 #data/settings only
-zip -r aurora-cloud.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION favicon.ico robots.txt composer.json modules.json -x **/*.bak
+zip -r package.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION favicon.ico robots.txt composer.json modules.json -x **/*.bak
 
 
 cd ./build
