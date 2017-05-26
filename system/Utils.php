f88e29c8112822b12d4072a52506df0fb0d8b6de
sash04ek <a.ovcharov@gmail.com>
2017-05-26 14:52:18 +0300

    namespaces refactoring in Storages

--- system/Utils.php
+++ system/Utils.php
@@ -2175,65 +2175,6 @@ class Utils
 /**
  * @package Api
  */
-class Validate
-{
-	/**
-	 * @param string $sFuncName
-	 * @return string
-	 */
-	public static function GetError($sFuncName)
-	{
-		switch ($sFuncName)
-		{
-			case 'Port':
-				return 'Required valid port.';
-			case 'IsEmpty':
-				return 'Required fields cannot be empty.';
-		}
-
-		return 'Error';
-	}
-
-	/**
-	 * @param mixed $mValue
-	 * @return bool
-	 */
-	public static function IsValidLogin($mValue)
-	{
-		return preg_match('/^[a-zA-Z0-9@\-_\.]+$/', $mValue);
-	}
-
-	/**
-	 * @param mixed $mValue
-	 * @return bool
-	 */
-	public static function IsEmpty($mValue)
-	{
-		return !is_string($mValue) || 0 === strlen($mValue);
-	}
-
-	/**
-	 * @param mixed $mValue
-	 * @return bool
-	 */
-	public static function Port($mValue)
-	{
-		$bResult = false;
-		if (0 < strlen((string) $mValue) && is_numeric($mValue))
-		{
-			$iPort = (int) $mValue;
-			if (0 < $iPort && $iPort < 65355)
-			{
-				$bResult = true;
-			}
-		}
-		return $bResult;
-	}
-}
-
-/**
- * @package Api
- */
 class Ints
 {
 	/**
