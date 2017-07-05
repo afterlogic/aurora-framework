6264af0d4fa92b62df65524e8d1e83b87f2d7086
vasil-sokolov <vasil.sokolov@gmail.com>
2017-07-05 12:37:50 +0300

    Create Installer.php

---
+++ Installer.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Aurora;
+
+use Composer\Script\Event;
+
+class Installer
+{
+    public static function postUpdate(Event $event)
+    {
+		
+		$sBaseDir = dirname(__File__);
+		$sConfigPath = dirname($sBaseDir) . '/pre-config.json';
+		$sMessage = "Configuration was updated successfully";
+		
+		if (file_exists($sConfigPath))
+		{
+			$sPreConfig = file_get_contents($sConfigPath);
+
+			$oPreConfig = json_decode($sPreConfig, true);
+
+			if ($oPreConfig)
+			{
+				include_once $sBaseDir . '/autoload.php';
+				
+				\Aurora\System\Api::Init();
+				
+				if ($oPreConfig['modules'])
+				{
+					foreach ($oPreConfig['modules'] as $sModuleName => $oModuleConfig)
+					{
+						foreach ($oModuleConfig as $sConfigName => $mConfigValue)
+						{
+							$oModuleManager = \Aurora\System\Api::GetModuleManager();
+
+							$mValue = $oModuleManager->getModuleConfigValue($sModuleName, $sConfigName, null);
+							if ($mValue !== null)
+							{
+								$oModuleManager->setModuleConfigValue($sModuleName, $sConfigName, $mConfigValue);
+								$oModuleManager->saveModuleConfigValue($sModuleName);
+							}
+							else
+							{
+								echo 'Invalid setting \'' . $sConfigName . '\' in module \''.$sModuleName.'\'';
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				$sMessage = "Invalid config file";
+			}
+		}
+		else
+		{	
+			$sMessage = "Config file didn't found";
+		}
+
+		echo $sMessage;
+    }
+
+}
