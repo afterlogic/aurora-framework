6867be76509630c8856752cb2144cc2bb296613e
artemij-nemec <artem@chesnokov.pp.ua>
2018-11-07 15:56:08 +0300

    Added AbstractAccount class

---
+++ Classes/AbstractAccount.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aurora\System\Classes;
+
+/**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
+ * @copyright Copyright (c) 2018, Afterlogic Corp.
+ *
+ * @package Api
+ */
+abstract class AbstractAccount extends \Aurora\System\EAV\Entity
+{
+	abstract public function getLogin();
+}
