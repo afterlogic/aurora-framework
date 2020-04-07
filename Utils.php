905d276876d01117aedb644d3d4a7ce9c8c9f34a
sash04ek <a.ovcharov@gmail.com>
2020-04-07 11:46:56 +0300

    fixed getClientIp method

--- Utils.php
+++ Utils.php
@@ -2118,28 +2118,28 @@ class Utils
 	public static function getClientIp()
 	{
 		$ipaddress = '';
-		
-		if ($_SERVER['HTTP_CLIENT_IP'])
+
+		if (isset($_SERVER['HTTP_CLIENT_IP']))
 		{
 			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
 		}
-		else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+		else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
 		{
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		}
-		else if($_SERVER['HTTP_X_FORWARDED'])
+		else if(isset($_SERVER['HTTP_X_FORWARDED']))
 		{
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
 		}
-		else if($_SERVER['HTTP_FORWARDED_FOR'])
+		else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
 		{
 			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
 		}
-		else if($_SERVER['HTTP_FORWARDED'])
+		else if(isset($_SERVER['HTTP_FORWARDED']))
 		{
 			$ipaddress = $_SERVER['HTTP_FORWARDED'];
 		}
-		else if($_SERVER['REMOTE_ADDR'])
+		else if(isset($_SERVER['REMOTE_ADDR']))
 		{
 			$ipaddress = $_SERVER['REMOTE_ADDR'];
 		}
