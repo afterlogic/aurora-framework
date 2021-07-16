845947b5bbf5128d234a09992ad34e703f3c88b0
sash04ek <a.ovcharov@afterlogic.com>
2021-07-16 16:09:38 +0300

    added is_cli() method

--- Utils.php
+++ Utils.php
@@ -2168,6 +2168,21 @@ class Utils
 
 		return $ipaddress;
 	}
+
+	public static function is_cli()
+	{
+		if( defined('STDIN') )
+		{
+			return true;
+		}
+		
+		if( empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0) 
+		{
+			return true;
+		} 
+		
+		return false;
+	}
 }
 
 /**
