e43790a8aad7edf1a5bc592918ac300567ec732c
sash04ek <a.ovcharov@afterlogic.com>
2022-12-08 14:14:08 +0300

    fixed BUG - db migration fails with fatal error

--- Api.php
+++ Api.php
@@ -1700,7 +1700,7 @@ class Api
                 $app->add(new Commands\Seeds\SeederMakeCommand($c['filesystem'], $c['composer']));
 
                 $app->add(new Commands\Migrations\EavToSqlCommand());
-				$app->add(new Commands\Migrations\EavToSqlCommandV1());
+//				$app->add(new Commands\Migrations\EavToSqlCommandV1());
 				$app->add(new Commands\GetOrphansCommand());
 
                 return $app;
