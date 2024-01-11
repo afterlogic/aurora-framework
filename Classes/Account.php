437ff269da6ec34d5938d9b65b4f0bea7fe6dc3c
sash04ek <a.ovcharov@gmail.com>
2024-01-11 11:18:25 +0300

    added Account class

---
+++ Classes/Account.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\System\Classes;
+
+class Account extends Model
+{
+    public function getLogin()
+    {
+        return '';
+    }
+
+    public function getPassword()
+    {
+        return '';
+    }
+}
