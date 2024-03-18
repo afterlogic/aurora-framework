56333af94c69cc6b1d4993c721813f4ede212fbb
Vasil Sokolov <vasil@afterlogic.com>
2024-03-18 16:08:20 +0200

    Added getClientIpInsecure method

--- Utils.php
+++ Utils.php
@@ -1992,6 +1992,27 @@ class Utils
         return $ipaddress;
     }
 
+    public static function getClientIpInsecure()
+    {
+        $ipaddress = '';
+
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ipaddress;
+    }
+
 }
 
 /**
