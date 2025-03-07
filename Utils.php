cc407fd1912f6c7ba3b160e8d27b97b3680fa273
sash04ek <a.ovcharov@gmail.com>
2025-03-07 14:24:06 +0300

    added getSizeFromIni method

--- Utils.php
+++ Utils.php
@@ -2002,6 +2002,26 @@ class Utils
         return $ipaddress;
     }
 
+    /**
+     * Summary of parse_size
+     * @param string $sizeName
+     * @return float size in bytes
+     */
+    public static function getSizeFromIni($sizeName) {
+        $size = ini_get($sizeName);
+        if ($size) {
+            $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+            $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+            if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+            } else {
+            return round($size);
+            }
+        } else {
+            return 0;
+        }
+    }
 }
 
 /**
